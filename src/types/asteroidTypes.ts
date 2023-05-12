3601a2df4073d6da4838531353bc0a2db7bad3c9
Sergi Casquero <sergicasquero@gmail.com>
2023-05-12 11:27:04 +0200

    Add AsteroidStructure interface

---
+++ src/types/asteroidTypes.ts
@@ -0,0 +1,9 @@
+export interface AsteroidStructure {
+  id: string;
+  name: string;
+  diameter: string;
+  hazardous: boolean;
+  speed: string;
+}
+
+export type AsteroidListStructure = AsteroidStructure[];
