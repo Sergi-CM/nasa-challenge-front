ef7e635ee37ac8778339408a146c73b75cb37a16
Sergi Casquero <sergicasquero@gmail.com>
2023-05-14 16:55:26 +0200

    Fix types in asteroidSlice, its tests and mocks

--- src/mocks/asteroids.ts
+++ src/mocks/asteroids.ts
@@ -1,18 +1,18 @@
-import { AsteroidListStructure } from "../types/asteroidTypes";
+import { AppAsteroidListStructure } from "../types/asteroidTypes";
 
-export const mockedAsteroids: AsteroidListStructure = [
+export const mockedAsteroids: AppAsteroidListStructure = [
   {
     id: "2465633",
     name: "465633 (2009 JR5)",
     maxDiameter: 476.474846455,
-    speed: "18.1279370671",
+    speed: "19.7498128142",
     hazardous: true,
   },
 
   {
     id: "3426410",
     name: "(2008 QV11)",
-    maxDiameter: 320.6564489709,
+    maxDiameter: 476.474846455,
     speed: "19.7498128142",
     hazardous: false,
   },
--- src/store/features/asteroid/asteroidSlice.test.tsx
+++ src/store/features/asteroid/asteroidSlice.test.tsx
@@ -1,18 +1,18 @@
 import { mockedAsteroids } from "../../../mocks/asteroids";
-import { AsteroidListStructure } from "../../../types/asteroidTypes";
+import { AppAsteroidListStructure } from "../../../types/asteroidTypes";
 import { asteroidReducer, loadAsteroidsActionCreator } from "./asteroidSlice";
 
 describe("Given a loadAsteroids reducer", () => {
   describe("When called with a list of two asteroids", () => {
     test("Then it should return a list with these two asteroids", () => {
-      const asteroidsInitialState: AsteroidListStructure = [];
+      const asteroidsInitialState: AppAsteroidListStructure = [];
 
       const newAsteroidsList = asteroidReducer(
         asteroidsInitialState,
         loadAsteroidsActionCreator(mockedAsteroids)
       );
 
-      expect(newAsteroidsList).toEqual(mockedAsteroids);
+      expect(newAsteroidsList).toStrictEqual(mockedAsteroids);
     });
   });
 });
