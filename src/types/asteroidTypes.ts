f3f44bd0e0efdfadca35b96c58a8880dd8eee3da
Sergi Casquero <sergicasquero@gmail.com>
2023-05-14 16:53:34 +0200

    Fix asteroid types to make them match the api data structure

--- src/types/asteroidTypes.ts
+++ src/types/asteroidTypes.ts
@@ -1,9 +1,33 @@
-export interface AsteroidStructure {
+export interface ApiAsteroidStructure {
   id: string;
   name: string;
-  maxDiameter: number;
+  estimated_diameter: {
+    kilometers: {
+      estimated_diameter_max: number;
+    };
+  };
+  is_potentially_hazardous_asteroid: boolean;
+  close_approach_data: [
+    {
+      relative_velocity: {
+        kilometers_per_second: string;
+      };
+    }
+  ];
+}
+
+export type ApiAsteroidListStructure = ApiAsteroidStructure[];
+
+export interface ApiResponseStructure {
+  near_earth_objects: { [key: string]: ApiAsteroidListStructure };
+}
+
+export interface AppAsteroidStructure {
+  id: string;
   hazardous: boolean;
+  maxDiameter: number;
+  name: string;
   speed: string;
 }
 
-export type AsteroidListStructure = AsteroidStructure[];
+export type AppAsteroidListStructure = AppAsteroidStructure[];
