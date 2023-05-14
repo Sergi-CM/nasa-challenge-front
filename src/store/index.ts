c6dfb66c4166332beb9de6cae163351e47132075
Sergi Casquero <sergicasquero@gmail.com>
2023-05-14 16:47:44 +0200

    Add datesReducer to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { asteroidReducer } from "./features/asteroid/asteroidSlice";
+import { datesReducer } from "./features/dates/datesSlice";
 
 export const store = configureStore({
   reducer: {
     asteroid: asteroidReducer,
+    dates: datesReducer,
   },
 });
 
